a131115938f2515c76d90b3b3530a83d58503bbf
malenpinto <malenpinto9@gmail.com>
2024-06-21 23:02:52 -0300

    iniciar sesion, registrarse

---
+++ app/Views/frontend/login.php
@@ -0,0 +1,23 @@
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="form-group">
+
+                <label for="email" class="col-sm-2 col-form-label cambio-letra-uno">Email</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="email" placeholder="Escriba su email">
+                </div>
+                <label for="password" class="col-sm-2 col-form-label cambio-letra-uno">Contraseña</label>
+                <div class="col-sm-10">
+                    <input type="password" class="form-control" id="password" placeholder="Escriba su contraseña">
+                </div>
+                <div class="col-auto mt-3">
+                    <button type="submit" class="btn btn-outline-dark cambio-letra-uno mb-3"> · Iniciar Sesión ·</button>
+                </div>
+            </div>
+            <h5 class="mt-4 mb-5 text-center cambio-letra-uno">¿No tenés cuenta? Registrate acá: <a class="no-underline " href="<?php echo base_url('registrarse'); ?>">
+                    <p class="d-inline" style="color: #5b8faf;">Crear cuenta </p><i class="fa-regular fa-hand-point-left"></i>
+                </a></h5>
+        </div>
+    </div>
+</div>
---
+++ app/Views/frontend/registrarse.php
@@ -0,0 +1,38 @@
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="nombre" class="col-sm-2 mt-1 col-form-label cambio-letra-uno">Nombre</label>
+                <div class="col-sm-10">
+                    <input type="name" class="form-control" id="nombre" placeholder="Escriba su nombre">
+                </div>
+                <label for="apellido" class="col-sm-2 mt-1 col-form-label cambio-letra-uno">Apellido</label>
+                <div class="col-sm-10">
+                    <input type="name" class="form-control" id="apellido" placeholder="Escriba su apellido">
+                </div>
+                <label for="celular" class="col-sm-2 mt-1 col-form-label cambio-letra-uno">Celular</label>
+                <div class="col-sm-10">
+                    <input type="num" class="form-control" id="celular" placeholder="Escriba su celular">
+                </div>
+                <label for="email" class="col-sm-2 mt-1 col-form-label cambio-letra-uno">Email</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="email" placeholder="Escriba su email">
+                </div>
+                <label for="password" class="col-sm-2 mt-1 col-form-label cambio-letra-uno">Contraseña</label>
+                <div class="col-sm-10">
+                    <input type="password" class="form-control" id="password" placeholder="Escriba su contraseña">
+                </div>
+                <label for="repassword" class="col-sm-2 mt-1 col-form-label cambio-letra-uno d-inline">Repetir contraseña</label>
+                <div class="col-sm-10">
+                    <input type="password" class="form-control" id="repassword" placeholder="Repita su contraseña">
+                </div>
+                <div class="col-auto mt-3">
+                    <button type="submit" class="btn btn-outline-dark cambio-letra-uno mb-3"> · Crear Cuenta ·</button>
+                </div>
+            </div>
+            <h5 class="mt-4 mb-5 text-center cambio-letra-uno">¿Ya tenés cuenta? Ingresa acá: <a class="no-underline " href="<?php echo base_url('iniciarsesion'); ?>">
+                    <p class="d-inline" style="color: #5b8faf;">Iniciar sesión </p><i class="fa-regular fa-hand-point-left"></i>
+                </a></h5>
+        </div>
+    </div>
+</div>
