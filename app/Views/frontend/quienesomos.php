bc85c2e7fa9d744b9bcda19f552e2db7c16049aa
malenpinto <malenpinto9@gmail.com>
2024-06-22 00:57:21 -0300

    navcorregido

--- app/Views/frontend/quienesomos.php
+++ app/Views/frontend/quienesomos.php
@@ -1,27 +1,27 @@
 <h2 class="text-center cambio-letra-uno mt-3 mb-3">Nuestro Equipo</h2>
-<div class="container">
-    <div class="row mb-3">
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="<?php echo base_url('assets/img/gerente.jpg'); ?>" class="card-img-top" alt="">
+<div class="container mb-3">
+    <div class="row">
+        <div class="col-s-12 col-md-4 d-flex justify-content-center">
+            <div class="card" style="width: 20rem;">
+                <img src="<?php echo base_url('assets/img/gerente.jpg'); ?>" class="card-img-top img-fluid" alt="">
                 <div class="card-body">
                     <p class="card-title cambio-letra-uno">Gerente.</p>
                     <p class="card-text cambio-letra-dos">Torres Damián</p>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="<?php echo base_url('assets/img/cordinadora.jpg'); ?>" class="card-img-top" alt="">
+        <div class="col-s-12 col-md-4 d-flex justify-content-center">
+            <div class="card" style="width: 20rem;">
+                <img src="<?php echo base_url('assets/img/cordinadora.jpg'); ?>" class="card-img-top img-fluid" alt="">
                 <div class="card-body">
                     <p class="card-title cambio-letra-uno">Coordinadora.</p>
                     <p class="card-text cambio-letra-dos">Sanchez Marina</p>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="<?php echo base_url('assets/img/marketing.jpg'); ?>" class="card-img-top" alt="">
+        <div class="col-s-12 col-md-4 d-flex justify-content-center">
+            <div class="card" style="width: 20rem;">
+                <img src="<?php echo base_url('assets/img/marketing.jpg'); ?>" class="card-img-top img-fluid" alt="">
                 <div class="card-body">
                     <p class="card-title cambio-letra-uno">Marketing Manager.</p>
                     <p class="card-text cambio-letra-dos">Rodriguez Lorena</p>
--- app/Views/plantillas/navbar.php
+++ app/Views/plantillas/navbar.php
@@ -1,40 +1,25 @@
-<!-- <nav class="navbar navbar-expand-lg sticky-top">
-    <ul class="navbar-nav text-center">
-        <a class="navbar-brand logo tipoLetraLogo" href="principal.html">·UTOPIA·TRAVEL·</a>
-        <li class="nav-item">
-            <a class="nav-link active tipoLetra" aria-current="page" href="principal.html">Inicio</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link tipoLetra" href="#">Quiénes somos</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link tipoLetra" href="#">Contacto</a>
-        </li>
-    </ul>
-</nav> -->
-
 <nav class="navbar navbar-expand-md navbar-nav sticky-top miBoton">
     <div class="d-flex justify-content-center align-content-center">
         <div class="row">
             <div class="col d-none d-md-block text-center">
                 <ul>
-                    <li><a class="navbar-brand logo tipoLetraLogo" href="principal.html">·UTOPIA·TRAVEL·</a></li>
+                    <li><a class="navbar-brand tipoLetraLogo ms-5" href="<?= base_url() ?>">·UTOPIA·TRAVEL·</a></li>
 
                     <li>
-                        <form class="d-flex w-50" role="search">
-                            <input class="form-control me-2" type="search" style="width: 550px;" placeholder="¿Qué estás buscando?" aria-label="Search">
-                            <button class="btn btn-outline" type="search"><i class="fa-solid fa-magnifying-glass fa-sm"></i></button>
+                        <form class="d-flex w-50 ms-5" role="search">
+                            <input class="form-control" type="search" style="width: 550px;" placeholder="¿Qué estás buscando?" aria-label="Search">
+                            <button class="btn btn-outline"><i class="fa-solid fa-magnifying-glass fa-sm"></i></button>
                         </form>
                     </li>
-                    <div class="collapse navbar-collapse me-2 d-none d-md-block ms-5" id="navbarSupportedContent">
+                    <div class="collapse navbar-collapse d-none d-md-block" id="navbarSupportedContent">
                         <ul class="navbar-nav d-flex align-items-center">
                             <li class="nav-item me-2">
-                                <a class="nav-link <?php echo (current_url() == base_url()) ? 'active-2' : ''; ?> tipoLetra" aria-current="page" href="<?= base_url() ?>">Inicio</a>
+                                <a class="nav-link <?php echo (current_url() == base_url()) ? 'active-2' : ''; ?> tipoLetra" aria-current="page" href="<?= base_url(); ?>">Inicio</a>
                             </li>
                             <li class="nav-item me-2">
                                 <a class="nav-link <?php echo (current_url() == base_url('quienessomos')) ? 'active-2' : ''; ?> tipoLetra" href="<?php echo base_url('quienessomos'); ?>">Quiénes Somos</a>
                             </li>
-                            <li class="nav-item me-5">
+                            <li class="nav-item">
                                 <a class="nav-link <?php echo (current_url() == base_url('contacto')) ? 'active-2' : ''; ?> tipoLetra" href="<?php echo base_url('contacto'); ?>">Contacto</a>
                             </li>
                             <li class="nav-item ms-5">
@@ -48,20 +33,20 @@
                                 </a>
                             </li>
                         </ul>
-
+                    </div>
                 </ul>
             </div>
         </div>
     </div>
     <div class="container-fluid d-md-none justify-content-start">
-        <a class="navbar-brand logo tipoLetraLogo" href="principal.html">·UTOPIA·TRAVEL·</a>
-        <button class="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand tipoLetraLogo" href="<?= base_url() ?>">·UTOPIA·TRAVEL·</a>
+        <form class="d-flex w-50" role="search">
+            <input class="form-control " type="search" style="width: 200px;" placeholder="¿Qué estás buscando?" aria-label="Search">
+            <button class="btn btn-outline me-4"><i class="fa-solid fa-magnifying-glass fa-sm me-4"></i></button>
+        </form>
+        <button class="navbar-toggler justify-content-end ms-5" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <form class="d-flex w-50" role="search">
-            <input class="form-control me-2" type="search" placeholder="¿Qué estás buscando?" aria-label="Search">
-            <button class="btn btn-outline" type="search"><i class="fa-solid fa-magnifying-glass fa-sm"></i></button>
-        </form>
         <div class="collapse navbar-collapse text-start ms-5" id="navbarSupportedContent">
             <ul class="navbar-nav mb-2 mb-lg-0 d-md-none">
                 <li class="nav-item">
